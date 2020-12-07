f8abff71acdbf7d079ff2f2bf76e0ea59c62c5a4
Alex <bozeak@gmail.com>
2020-12-07 09:31:09 +0200

    init

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Head from 'next/head'
+
+
+const about = () => {
+    return (
+        <div>
+            <Head>
+        <title>About | My page title</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+            About page
+        </div>
+    )
+}
+
+export default about
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,68 @@
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Image from 'next/image'
+import Head from 'next/head'
+import nodeFetch from 'node-fetch';
+import { createApi } from 'unsplash-js';
+import { render } from 'react-dom';
+// import { getStaticProps } from 'next'
 
-export default function Home() {
+// let resultsA = [];
+// const unsplash = createApi({
+//   accessKey: 'K4ZFq6v5ja8e8pXEJDI_X7v1aQS82VClS8nQCDhkIGo',
+//   fetch: nodeFetch,
+// });
+
+
+// let images = unsplash.photos.list()
+//   .then(result => {
+//     // console.log(result);
+//     const feed = result.response;
+
+//     const { total, results } = feed;
+
+//     // extract total and results array from response
+//     // return results;
+//   });
+
+
+// console.log(images);
+
+export async function getStaticProps(context) {
+  const unsplash = createApi({
+    accessKey: 'K4ZFq6v5ja8e8pXEJDI_X7v1aQS82VClS8nQCDhkIGo',
+    fetch: nodeFetch,
+  });
+
+  const res = await unsplash.search.getPhotos({
+    page: 4,
+    perPage: 16,
+    query: 'cat'
+  });
+  const images = await res.response;
+
+  // console.log(images);
+
+  return {
+    props: {
+      images,
+    }
+  }
+
+}
+
+export default function Home({images}) {
   return (
+
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>My page title</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
+      <h2>Hello</h2>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      {images.results.map((post) => (
+        <Image src={post.urls.full} alt={post.alt_description} width={post.width} height={post.height} key={post.id} />
+      ))}
     </div>
   )
 }
